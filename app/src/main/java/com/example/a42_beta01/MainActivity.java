bc45555661bb83e455ec2362970c9ccf3c95fdfc
sandeeppadala <spa@gmai.com>
2020-11-24 10:52:24 +0530

    backport changed main act

--- app/src/main/java/com/example/a42_beta01/MainActivity.java
+++ app/src/main/java/com/example/a42_beta01/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                     binding.fab.setOnClickListener(new View.OnClickListener() {
                               @Override
                               public void onClick(View view) {
-                                        Snackbar.make(view, " Hii Replace with your own actionhiiii", Snackbar.LENGTH_LONG)
+                                        Snackbar.make(view, "Replace with your own actionhiiii", Snackbar.LENGTH_LONG)
                                                 .setAction("Action", null).show();
                               }
                     });
